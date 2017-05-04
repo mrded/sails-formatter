c4ed4009ac204ad115b0b7b6ee82e555e5419c59
Dmitry Demenchuk <dmitry@demenchuk.me>
2017-05-04 11:59:04 +0100

    Add possibility to load multiple fields at once.

--- index.js
+++ index.js
@@ -20,5 +20,20 @@ var many = function(objects, model, type) {
   return Promise.all(output);
 };
 
+var load = function(fields, output) {
+  var outputPromises = fields.map(function(field) {
+    var formatter = Array.isArray(field.data) ? many : one;
+    
+    return formatter(field.data, field.model, field.type).then(function(refObjects) {
+      output[field.field] = refObjects;
+    });
+  });
+
+  return Promise.all(outputPromises).then(function() {
+    return output;
+  });
+};
+
 module.exports.one = one;
 module.exports.many = many;
+module.exports.load = load;
